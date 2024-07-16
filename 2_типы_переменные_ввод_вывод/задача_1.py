5026940aeaf2986b5c6d46a3957e3895c22cc3b7
Golandec-SK <golandec.spb@gmail.com>
2024-07-16 14:10:39 +0300

    main_sk

---
+++ 2_типы_переменные_ввод_вывод/задача_1.py
@@ -0,0 +1,13 @@
+# Допустимые
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"   # Верблюжий регистр - Каждое слово, кроме первого, начинается с заглавной буквы
+MYVAR = "John"
+myvar2 = "John"
+
+# Недопустимые
+2myvar = "John"
+my-var = "John"
+мой var = "John"
+-name = 'John'
---
+++ 2_типы_переменные_ввод_вывод/задача_2.py
@@ -0,0 +1,8 @@
+# Ваше любимое блюдо
+favorite_food = input("\nВведите ваше любимое блюдо: ")
+favorite_food2 = input("\nВведите еще одно любимое блюдо: ")
+food = favorite_food + ' ' + favorite_food2
+
+print('\n', food)
+
+input("\nНажмите Enter, чтобы выйти.\n")
